7a6e5854451b0c21eef320561b258ae54f4f2a6f
Shane O'Brien <shane.g.obrien@gmail.com>
2024-01-23 13:19:57 -0500

    added routes

--- src/App/App.js
+++ src/App/App.js
@@ -3,15 +3,18 @@ import About from "../About/About";
 import Projects from "../Projects/Projects";
 import Resume from "../Resume/Resume";
 import Footer from "../Footer/Footer";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <About />
-      <Projects />
-      <Resume />
+      <Routes>
+        <Route path="*" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/resume" element={<Resume />} />
+      </Routes>
       <Footer />
     </div>
   );
