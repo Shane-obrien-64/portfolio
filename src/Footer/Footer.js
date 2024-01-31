287e411613db922265898b879107248b744dc101
Shane O'Brien <shane.g.obrien@gmail.com>
2024-01-31 10:11:05 -0500

    minor edits

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,11 +1,11 @@
+import Contact from "../Contact/Contact";
 import "./Footer.css";
 
 const Footer = () => {
   return (
-    <section className="footer app__section">
-      <div>Footer</div>
-      <div>Footer</div>
-      <div>Footer</div>
+    <section className="footer">
+      <p className="footer__text">Developed by Shane O'Brien 2024</p>
+      <Contact />
     </section>
   );
 };
