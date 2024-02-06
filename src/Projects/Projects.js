9cc8f247a75f8aad8428ad8d2d893b09624c80a6
Shane O'Brien <shane.g.obrien@gmail.com>
2024-02-06 13:54:10 -0500

    added markup and styles to projects

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -3,9 +3,12 @@ import "./Projects.css";
 const Projects = () => {
   return (
     <section className="projects app__section">
-      <div>Projects</div>
-      <div>Projects</div>
-      <div>Projects</div>
+      <ul className="projects__list">
+        <li className="projects__item">box 1</li>
+        <li className="projects__item">box 2</li>
+        <li className="projects__item">box 3</li>
+        <li className="projects__item">box 4</li>
+      </ul>
     </section>
   );
 };
