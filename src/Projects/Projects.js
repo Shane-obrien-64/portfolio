d7dd2fa38b5ef5db0582d37662e2de5804193dcb
Shane O'Brien <shane.g.obrien@gmail.com>
2024-02-19 13:48:54 -0500

    added project screenshots

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -1,6 +1,9 @@
 import wtwr from "../Images/wtwr.svg";
 import inDex from "../Images/inDex.svg";
 import aroundtheUS from "../Images/aroundtheUS.svg";
+import wtwrSS from "../Images/wtwr_sc.png";
+import atusSS from "../Images/atus_sc.png";
+import indexSS from "../Images/index_sc.png";
 import "./Projects.css";
 
 const Projects = () => {
@@ -12,18 +15,21 @@ const Projects = () => {
             <h2 className="projects__item-title">WTWR</h2>
             <img src={wtwr} className="projects__item-logo" />
           </div>
+          <img className="projects__image" src={wtwrSS} />
         </li>
         <li className="projects__item">
           <div className="projects__item-container">
             <h2 className="projects__item-title">inDex</h2>
             <img src={inDex} className="projects__item-logo" />
           </div>
+          <img className="projects__image" src={indexSS} />
         </li>
         <li className="projects__item">
           <div className="projects__item-container">
             <h2 className="projects__item-title">Around the US</h2>
             <img src={aroundtheUS} className="projects__item-logo" />
           </div>
+          <img className="projects__image" src={atusSS} />
         </li>
       </ul>
     </section>
