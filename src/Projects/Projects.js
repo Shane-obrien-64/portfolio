b877e7c8a2ccfb00e18ade6113179fad0e75e182
Shane O'Brien <shane.g.obrien@gmail.com>
2024-02-19 13:14:49 -0500

    added logo images

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -1,13 +1,30 @@
+import wtwr from "../Images/wtwr.svg";
+import inDex from "../Images/inDex.svg";
+import aroundtheUS from "../Images/aroundtheUS.svg";
 import "./Projects.css";
 
 const Projects = () => {
   return (
     <section className="projects app__section">
       <ul className="projects__list">
-        <li className="projects__item">box 1</li>
-        <li className="projects__item">box 2</li>
-        <li className="projects__item">box 3</li>
-        <li className="projects__item">box 4</li>
+        <li className="projects__item">
+          <div className="projects__item-container">
+            <h2 className="projects__item-title">WTWR</h2>
+            <img src={wtwr} className="projects__item-logo" />
+          </div>
+        </li>
+        <li className="projects__item">
+          <div className="projects__item-container">
+            <h2 className="projects__item-title">inDex</h2>
+            <img src={inDex} className="projects__item-logo" />
+          </div>
+        </li>
+        <li className="projects__item">
+          <div className="projects__item-container">
+            <h2 className="projects__item-title">Around the US</h2>
+            <img src={aroundtheUS} className="projects__item-logo" />
+          </div>
+        </li>
       </ul>
     </section>
   );
