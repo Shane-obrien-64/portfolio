57998ca20397c98923e0aca5002d28e04e9f8acc
Shane O'Brien <shane.g.obrien@gmail.com>
2024-02-21 17:16:00 -0500

    changed item classes to card

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -10,24 +10,24 @@ const Projects = () => {
   return (
     <section className="projects app__section">
       <ul className="projects__list">
-        <li className="projects__item">
-          <div className="projects__item-container">
-            <h2 className="projects__item-title">WTWR</h2>
-            <img src={wtwr} className="projects__item-logo" />
+        <li className="projects__card">
+          <div className="projects__card-container">
+            <h2 className="projects__card-title">WTWR</h2>
+            <img src={wtwr} className="projects__card-logo" />
           </div>
           <img className="projects__image" src={wtwrSS} />
         </li>
-        <li className="projects__item">
-          <div className="projects__item-container">
-            <h2 className="projects__item-title">inDex</h2>
-            <img src={inDex} className="projects__item-logo" />
+        <li className="projects__card">
+          <div className="projects__card-container">
+            <h2 className="projects__card-title">inDex</h2>
+            <img src={inDex} className="projects__card-logo" />
           </div>
           <img className="projects__image" src={indexSS} />
         </li>
-        <li className="projects__item">
-          <div className="projects__item-container">
-            <h2 className="projects__item-title">Around the US</h2>
-            <img src={aroundtheUS} className="projects__item-logo" />
+        <li className="projects__card">
+          <div className="projects__card-container">
+            <h2 className="projects__card-title">Around the US</h2>
+            <img src={aroundtheUS} className="projects__card-logo" />
           </div>
           <img className="projects__image" src={atusSS} />
         </li>
