7a8612d041ded0471bdeaf0f4fbcdd20cb061fd8
Shane O'Brien <shane.g.obrien@gmail.com>
2024-04-18 17:48:51 -0400

    added links to modal images

--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -1,22 +1,27 @@
 import "./Modal.css";
 
 const Modal = ({ closeModal, selectedCard }) => {
+  const handleLink = () => {};
   return (
     <div className="modal" onClick={closeModal}>
       <div className="modal__container">
         <div className="modal__header">
           <h2 className="modal__title">{selectedCard.title}</h2>
-          <img
-            src={selectedCard.logoSrc}
-            className="projects__card-logo"
-            alt={`${selectedCard.title} Logo`}
-          />
+          <a href={selectedCard.link} target="_blank">
+            <img
+              src={selectedCard.logoSrc}
+              className="modal__logo"
+              alt={`${selectedCard.title} Logo`}
+            />
+          </a>
         </div>
         <p className="modal__text">{selectedCard.text}</p>
         <p className="modal__text modal__text_bold">
           Tech: {selectedCard.tech}
         </p>
-        <img className="modal__img" src={selectedCard.imgSrc} />
+        <a href={selectedCard.link} target="_blank">
+          <img className="modal__img" src={selectedCard.imgSrc} />
+        </a>
       </div>
     </div>
   );
