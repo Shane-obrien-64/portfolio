f77c08507697cd437ca3a07ccae28242ad2cba8a
Shane O'Brien <shane.g.obrien@gmail.com>
2024-04-18 17:39:17 -0400

    added logos to modal

--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -4,7 +4,14 @@ const Modal = ({ closeModal, selectedCard }) => {
   return (
     <div className="modal" onClick={closeModal}>
       <div className="modal__container">
-        <h2 className="modal__title">{selectedCard.title}</h2>
+        <div className="modal__header">
+          <h2 className="modal__title">{selectedCard.title}</h2>
+          <img
+            src={selectedCard.logoSrc}
+            className="projects__card-logo"
+            alt={`${selectedCard.title} Logo`}
+          />
+        </div>
         <p className="modal__text">{selectedCard.text}</p>
         <p className="modal__text modal__text_bold">
           Tech: {selectedCard.tech}
