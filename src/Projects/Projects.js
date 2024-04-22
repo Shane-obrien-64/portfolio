ef8371851690920f98fb4bdc92617f69030f86d2
Shane O'Brien <shane.g.obrien@gmail.com>
2024-04-22 15:46:13 -0400

    added const file and move cardData const to it

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -1,39 +1,7 @@
-import wtwrLogo from "../Images/wtwr.svg";
-import inDexLogo from "../Images/inDex.svg";
-import aroundtheUSLogo from "../Images/aroundtheUS.svg";
-import wtwrSS from "../Images/wtwr_sc.png";
-import atusSS from "../Images/atus_sc.png";
-import indexSS from "../Images/index_sc.png";
+import cardData from "../utils/const";
 import "./Projects.css";
 
 const Projects = ({ handleSelectedCard }) => {
-  const cardData = [
-    {
-      title: "WTWR",
-      logoSrc: wtwrLogo,
-      imgSrc: wtwrSS,
-      link: "https://github.com/Shane-obrien-64/se_project_react",
-      tech: "React.js, Node.js, Express.js, MongoDB, CSS3",
-      text: "Seamlessly blending fashion and forecast, WTWR helps users dress appropriately for the weather by curating outfits from their wardrobe. Its frontend, built with React.js, ensures a user-friendly experience, while the robust backend, powered by MongoDB and Express.js, delivers accurate recommendations.",
-    },
-    {
-      title: "inDex",
-      logoSrc: inDexLogo,
-      imgSrc: indexSS,
-      link: "https://github.com/Shane-obrien-64/inDex-frontend",
-      tech: "React.js, CSS3",
-      text: "Showcasing a fusion of creativity and functionality, InDex is a dynamic single-page application proficiently crafted with React.js and CSS3. Demonstrating expertise in API integration, it elegantly displays data from third-party sources while employing adaptive design principles for an optimized user experience.",
-    },
-
-    {
-      title: "Around the US",
-      logoSrc: aroundtheUSLogo,
-      imgSrc: atusSS,
-      link: "https://github.com/Shane-obrien-64/se_project_aroundtheus",
-      tech: "JavaScript, CSS3, HTML5",
-      text: "Offering a vibrant platform reminiscent of Instagram, this web app invites users to share their photographic adventures across America. Mastering fundamental web development techniques, its frontend boasts responsive design and seamless integration of third-party APIs, showcasing foundational skills in JavaScript, CSS3, and HTML5.",
-    },
-  ];
   const handleCardClick = (card) => {
     handleSelectedCard(card);
   };
